6909bb1a1d2bf381ed834fbe000e1b6e65272cff
scottyvg <svangilder@gmail.com>
2017-03-12 21:26:57 -0600

    change db to DB

--- knexfile.js
+++ knexfile.js
@@ -3,15 +3,15 @@ module.exports = {
   development: {
     client: 'pg',
     connection: "postgres://localhost/LII",
-    migrations: { directory: "./db/migrations", },
-    seeds: { directory: "./db/seeds", }
+    migrations: { directory: "./DB/migrations", },
+    seeds: { directory: "./DB/seeds", }
   },
 
   production: {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}?ssl=true`,
-    migrations: { directory: "./db/migrations", },
-    seeds: { directory: "./db/seeds", }
+    migrations: { directory: "./DB/migrations", },
+    seeds: { directory: "./DB/seeds", }
   }
 
 };
