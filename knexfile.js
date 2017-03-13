b730a61ef353470cfd3967c58cc625a814921816
scottyvg <svangilder@gmail.com>
2017-03-12 21:24:52 -0600

    update package.json & knexfile

--- knexfile.js
+++ knexfile.js
@@ -1,19 +1,17 @@
-// Update with your config settings.
-
 module.exports = {
 
   development: {
     client: 'pg',
     connection: "postgres://localhost/LII",
-    migrations: { directory: "./db/migrations" },
-    seeds: { directory: "./db/seeds"}
+    migrations: { directory: "./db/migrations", },
+    seeds: { directory: "./db/seeds", }
   },
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
-    migrations: { directory: "./db/migrations" },
-    seeds: { directory: "./db/seeds"}
+    connection: `${process.env.DATABASE_URL}?ssl=true`,
+    migrations: { directory: "./db/migrations", },
+    seeds: { directory: "./db/seeds", }
   }
 
 };
